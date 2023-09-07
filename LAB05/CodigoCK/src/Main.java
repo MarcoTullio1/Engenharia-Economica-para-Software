89552d458da2955f4e3e011d83890b7513d18b59
Marco Tullio <64559501+MarcoTullio1@users.noreply.github.com>
2023-09-07 15:35:48 -0300

    Código Inicial

---
+++ LAB05/CodigoCK/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Isso é para exibir uma menssagem com meu nome
+        System.out.println("Meu nome: Marco");
+        // Isso é para exibir o nome da dsiciplina
+        System.out.println("Nome da disciplina: Engenharia Econômica para Software");
+    }
+}
